2ff616e17d0da8dec23554975baf16224de0e2c2
renato-mota <renato.mota@assertivasolucoes.com.br>
2018-06-09 17:23:24 -0300

    Teste com Maps
    Tesnta a integração com o react-native-maps

--- App.js
+++ App.js
@@ -6,38 +6,46 @@ import {
   TouchableOpacity
 } from 'react-native';
 import RNGooglePlaces from 'react-native-google-places';
-import { Container, Header, Content, Button, Text, Icon  } from 'native-base';
+import { Container, Header, Content, Button, Text, Icon, Body,Title, Left, Right  } from 'native-base';
 
 type Props = {};
 export default class App extends Component<Props> {
+
   state={
     place:''
   }
+
   openSearchModal() {
     RNGooglePlaces.openAutocompleteModal({useOverlay:true})
     .then((place) => {
 		this.setState({place:place})
-		
+		console.log(place)
     })
-    .catch(error => console.log(error.message));  // error is a Javascript Error object
+    .catch(error => console.log(error.message));
   }
+
   render() {
     const {place} = this.state
     return (
       <Container stule={styles.container}>
         <Content>
+        <Header style={{margin:8, backgroundColor:'white'}}>
+        
+          <Body>
+            <Text  style={{color:'#acbcbb'}} onPress={() => this.openSearchModal()}>
+              Pres to pick a place
+            </Text>
+          </Body>  
+            <Right>
+              <Button transparent onPress={() => this.openSearchModal()}>
+              <Icon name='search' style={{color:'#4f6162'}}/>
+              </Button>
+            </Right>
+        </Header>
           <Text >
             {place ? JSON.stringify(place) : ''}
           </Text>
         </Content>
-          <Button
-              block
-              primary              
-              onPress={() => this.openSearchModal()}
-            >
-              <Text>Press to pick a place</Text>
-              <Icon name='pin' />
-          </Button>
       </Container>
     );
   }
