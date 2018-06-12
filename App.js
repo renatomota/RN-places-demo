1264d153e03cd3aff9519e24df0b1852ebeb7736
renato-mota <renato.mota@assertivasolucoes.com.br>
2018-06-11 23:22:40 -0300

    Adição do maps
    Google Maps funcionando com o places agora =D

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 import RNGooglePlaces from 'react-native-google-places';
 import { Container, Header, Content, Button, Text, Icon, Body,Title, Left, Right  } from 'native-base';
-
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 type Props = {};
 export default class App extends Component<Props> {
 
@@ -18,7 +18,11 @@ export default class App extends Component<Props> {
   openSearchModal() {
     RNGooglePlaces.openAutocompleteModal({useOverlay:true})
     .then((place) => {
-		this.setState({place:place})
+    this.setState({place:place})
+    this._map.fitToCoordinates([{latitude:place.latitude, longitude:place.longitude}], {
+      edgePadding: { top: 80, right: 80, bottom: 80, left: 80 },
+      animated: true,
+    });
 		console.log(place)
     })
     .catch(error => console.log(error.message));
@@ -26,14 +30,22 @@ export default class App extends Component<Props> {
 
   render() {
     const {place} = this.state
+    
     return (
-      <Container stule={styles.container}>
-        <Content>
+      <Container style={{flex:1}}>
+
+        <MapView
+          style={styles.map}
+          ref={ref => { this._map = ref; }}
+          provider={PROVIDER_GOOGLE} 
+          showsPointsOfInterest = {true}
+          showsCompass = {true}
+         
+        />
         <Header style={{margin:8, backgroundColor:'white'}}>
-        
           <Body>
-            <Text  style={{color:'#acbcbb'}} onPress={() => this.openSearchModal()}>
-              Pres to pick a place
+            <Text numberOfLines={1} style={{color:'#acbcbb', fontSize:14}} onPress={() => this.openSearchModal()}>
+              {place ? place.name :'Press to pick a place'}
             </Text>
           </Body>  
             <Right>
@@ -42,10 +54,9 @@ export default class App extends Component<Props> {
               </Button>
             </Right>
         </Header>
-          <Text >
-            {place ? JSON.stringify(place) : ''}
-          </Text>
-        </Content>
+        
+        
+       
       </Container>
     );
   }
@@ -53,8 +64,13 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
+    ...StyleSheet.absoluteFillObject,
+    flex:1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
   welcome: {
     fontSize: 20,
--- android/app/src/main/java/com/placesdemo/MainApplication.java
+++ android/app/src/main/java/com/placesdemo/MainApplication.java
@@ -24,8 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGooglePlacesPackage(),
-            new MapsPackage()
+            new MapsPackage(),
+            new RNGooglePlacesPackage()
       );
     }
 
